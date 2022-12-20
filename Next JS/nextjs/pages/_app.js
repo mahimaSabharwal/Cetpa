d5a680aced2ba368edcd8977121ec19bca9943a8
Mahima Dutta <mahima.dutta@altudo.co>
2022-12-20 10:18:47 +0530

    Fonts

--- Next JS/nextjs/pages/_app.js
+++ Next JS/nextjs/pages/_app.js
@@ -1,10 +1,17 @@
 import Layout from "../common/Layout";
 import Head from "next/head";
+import { Roboto } from "@next/font/google";
 
 import "bootstrap/dist/css/bootstrap.css";
 
 //Styles
 import "../styles/global.scss";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,7 +20,9 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Layout footerStatus={pageProps.footerStatus}>
-        <Component {...pageProps} />
+        <div className={roboto.className}>
+          <Component {...pageProps} />
+        </div>
       </Layout>
     </>
   );
